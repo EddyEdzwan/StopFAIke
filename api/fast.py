ce501f32b3c6b1867642b4efeaf52cb54142fc5f
Eddy Edzwan <eddyedzwan@gmail.com>
2021-10-01 14:09:06 +0800

    Add Model Explainability Notebook & update to requirements.txt + api/fast.py for endpoint to allow for model explainability

--- api/fast.py
+++ api/fast.py
@@ -1,7 +1,13 @@
 import numpy as np
 
+import pandas as pd
+
+import shap
+
 import tensorflow as tf
 import tensorflow_text as text
+
+from transformers import AutoTokenizer
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,6 +29,26 @@ app.add_middleware(
 # Get the model (locally or from GCP)
 reloaded_model = get_model_from_gcp(local=True)
 
+# Initialising the bert tokenizer for use in shap.Explainer
+model_name = "bert-base-uncased"
+bert_tokenizer_pretrained = AutoTokenizer.from_pretrained(model_name)
+
+#Creating custom function for shap.Explainer
+def f(x):
+    '''
+    Returns the class probabilities output by the custom BERT classifier - two classes  
+    '''
+    # Calculate the class probabilities and stack them for each input sentence
+    prob_class_0, prob_class_1 = 1 - reloaded_model(x), reloaded_model(x)
+    stacked_probabilities = np.hstack((prob_class_0, prob_class_1))
+    
+    # Calculate the logit values, change return value if choosing to explain via logits instead 
+    # val = sp.special.logit(stacked_probabilities)
+    
+    return stacked_probabilities
+
+# Initialising the shap.Explainer object
+explainer = shap.Explainer(f, masker=bert_tokenizer_pretrained, output_names=['TRUE', 'FAKE'])
 
 @app.get("/")
 def index():
@@ -45,3 +71,27 @@ def predict(article):
     print(type(X_clean))
 
     return {'prediction': float(y_prob.numpy()[0][0])}
+
+@app.get("/shapvalues")
+def shapvalues(article):
+
+    X = dict(article=article)
+
+    # cleaning
+    X_clean = clean(X['article'], stopword=False, lemmat=False)
+
+    # prediction
+    y_prob = reloaded_model([X_clean])
+
+    shap_values = explainer([X_clean])
+
+    if y_prob.numpy()[0][0] >= 0.5:
+        return {"values" : list(shap_values.values[0, :, 1]),
+        "base_values" : float(shap_values.base_values[0, 1]),
+        "data" : list(shap_values.data[0]),
+        "output_names" : str(shap_values.output_names[1])}
+    
+    return {"values" : list(shap_values.values[0, :, 0]),
+        "base_values" : float(shap_values.base_values[0, 0]),
+        "data" : list(shap_values.data[0]),
+        "output_names" : str(shap_values.output_names[0])}
